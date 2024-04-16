b2e760814692ab0be1d50bce179e7cb49dbbbd9f
Zeno Belligoli <zeno.belligoli@booking.com>
2024-04-16 02:12:55 +0200

    add extra test for api

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,30 +10,56 @@ def test_api_locally_get_root():
     assert r.json() == {"message": "Welcome to Zeno's ML model API!"}
 
 
-def test_api_locally_get_post_ok():
+def test_api_locally_get_post_ok_0():
     # Given payload from the cURL command
     payload = {
-        "age": [19, 37, 81],
-        "capital-gain": [0, 0, 1887],
-        "capital-loss": [0, 1567, 76],
-        "education": ["Some-college", "HS-grad", "Some-college"],
-        "education-num": [10, 9, 10],
-        "fnlgt": [212800, 185556, 182898],
-        "hours-per-week": [40, 35, 40],
-        "marital-status": ["Never-married", "Married-civ-spouse", "Married-civ-spouse"],
-        "native-country": ["United-States", "Italy", "United-States"],
-        "occupation": ["Sales", "Craft-repair", "Adm-clerical"],
-        "race": ["White", "White", "Asian"],
-        "relationship": ["Own-child", "Husband", "Other-relative"],
-        "salary": ["<=50K", "<=50K", ">50K"],
-        "sex": ["Female", "Male", "Female"],
-        "workclass": ["Private", "Private", "Federal-gov"],
+        "age": [19],
+        "capital-gain": [0],
+        "capital-loss": [0],
+        "education": ["Some-college"],
+        "education-num": [10],
+        "fnlgt": [212800],
+        "hours-per-week": [40],
+        "marital-status": ["Never-married"],
+        "native-country": ["United-States"],
+        "occupation": ["Sales"],
+        "race": ["White"],
+        "relationship": ["Own-child"],
+        "salary": ["<=50K"],
+        "sex": ["Female"],
+        "workclass": ["Private"],
     }
 
     # Make POST request to the endpoint
     r = client.post("/infer/", json=payload)
     assert r.status_code == 200
-    assert r.json() == {"predictions": "[0 1 0]"}
+    assert r.json() == {"predictions": "[0]"}
+
+
+def test_api_locally_get_post_ok_1():
+    # Given payload from the cURL command
+    payload = {
+        "age": [37],
+        "capital-gain": [0],
+        "capital-loss": [1567],
+        "education": ["HS-grad"],
+        "education-num": [9],
+        "fnlgt": [185556],
+        "hours-per-week": [35],
+        "marital-status": ["Married-civ-spouse"],
+        "native-country": ["Italy"],
+        "occupation": ["Craft-repair"],
+        "race": ["White"],
+        "relationship": ["Husband"],
+        "salary": ["<=50K"],
+        "sex": ["Male"],
+        "workclass": ["Private"],
+    }
+
+    # Make POST request to the endpoint
+    r = client.post("/infer/", json=payload)
+    assert r.status_code == 200
+    assert r.json() == {"predictions": "[1]"}
 
 
 def test_api_locally_get_post_not_ok():
